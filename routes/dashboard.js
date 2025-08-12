bf8ad22ef26222b36213462b9ee24114a70c0298
Jmate-2011 <7b7t.net@gmail.com>
2025-08-12 08:46:58 +0000

    update sidebar

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,6 +5,10 @@ router.get('/', (req, res) => {
   res.render('dashboard', { user: req.session.user });
 });
 
+router.get('/dashboard', (req, res) => {
+  res.render('parts/sidebar', { currentPath: req.path });
+});
+
 router.get('/logout', (req, res) => {
   req.session.destroy();
   res.redirect('/login');
